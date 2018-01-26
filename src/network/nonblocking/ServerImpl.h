8e517e61c8db52ce16d161d2f9a80d1a30595db6
annastasiya <35397835+annastasiya@users.noreply.github.com>
2018-01-26 12:54:18 +0300

    Add files via upload

--- src/network/nonblocking/ServerImpl.h
+++ src/network/nonblocking/ServerImpl.h
@@ -1,7 +1,40 @@
 #ifndef AFINA_NETWORK_NONBLOCKING_SERVER_H
 #define AFINA_NETWORK_NONBLOCKING_SERVER_H
 
+#include "Utils.h"
+
+#include <atomic>
+#include <pthread.h>
 #include <vector>
+
+#include <algorithm>
+#include <cassert>
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+
+#include <pthread.h>
+#include <signal.h>
+
+#include <errno.h>
+#include <sys/epoll.h>
+
+#include <netdb.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#include <afina/Storage.h>
+#include <afina/execute/Command.h>
+#include <protocol/Parser.h>
+
+#include <list>
 
 #include <afina/network/Server.h>
 
@@ -29,15 +62,31 @@ public:
 
     // See Server.h
     void Join() override;
+    friend void* Epoll_Proxy(void* p);
+
+protected:
+    /**
+    Method is calling epoll_wait in a separate thread
+    */
+    void RunEpoll();
 
 private:
     // Port to listen for new connections, permits access only from
     // inside of accept_thread
     // Read-only
-    uint32_t listen_port;
+    //uint32_t listen_port;
 
     // Thread that is accepting new connections
-    std::vector<Worker> workers;
+    //std::vector<Worker> workers;
+    std::atomic<bool> running;
+
+    // Thread accepting and crunching client connections
+    std::vector<pthread_t> workers;
+
+    // Port number to listen on
+    uint16_t listen_port;
+
+
 };
 
 } // namespace NonBlocking
--- src/network/nonblocking/Utils.cpp
+++ src/network/nonblocking/Utils.cpp
@@ -1,31 +1,39 @@
 #include "Utils.h"
-
 #include <stdexcept>
-
 #include <fcntl.h>
-#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <cassert>
+#include <iostream>
 
 namespace Afina {
 namespace Network {
 namespace NonBlocking {
 
-void make_socket_non_blocking(int sfd) {
-    int flags, s;
-
-    flags = fcntl(sfd, F_GETFL, 0);
-    if (flags == -1) {
-        throw std::runtime_error("Failed to call fcntl to get socket flags");
+int setsocknonblocking(int sock) {
+        int flags = fcntl(sock, F_GETFL, NULL);
+        if (flags == -1)
+            return flags;
+        flags |= O_NONBLOCK;
+        return fcntl(sock, F_SETFL, flags);
     }
 
-    flags |= O_NONBLOCK;
-    s = fcntl(sfd, F_SETFL, flags);
-    if (s == -1) {
-        throw std::runtime_error("Failed to call fcntl to set socket flags");
-    }
+void * Epoll_Proxy(void *p){
+    ServerImpl *srv = reinterpret_cast<ServerImpl *>(p);
+    srv->RunEpoll();
+    return 0;
+} 
+
+int epoll_modify(int epoll_fd, int how, uint32_t events, ep_fd &target) {
+    struct epoll_event new_ev {  // создаем структуры для epoll_ctl
+            events, { (void *)&target }
+    };
+    return epoll_ctl(epoll_fd, how, target.fd, &new_ev);
 }
+ 
+
+
 
 } // namespace NonBlocking
 } // namespace Network
